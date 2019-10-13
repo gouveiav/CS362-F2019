d57ce5ea5a0a02a811069a8e22478c0ff0b804cf
Vanessa Gouveia <gouveiav@oregonstate.edu>
2019-10-13 01:55:09 -0700

    Re-organized by alph and fixed a spelling error

--- projects/gouveiav/dominion/dominion_helpers.h
+++ projects/gouveiav/dominion/dominion_helpers.h
@@ -13,12 +13,12 @@ int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
 
 int verifyAmbassadorChoice(int choice1, int choice2, struct gameState *state, int handPos);
+int ambassadorCardEffect(int choice1, int choice2, struct gameState *state, int handPos);
 
 int baronCardEffect(int choice1, struct gameState *state);
-int minonCardEffect(int choice1, int choice2, struct gameState *state, int handPos);
-int ambassadorCardEffect(int choice1, int choice2, struct gameState *state, int handPos);
-int tributeCardEffect(struct gameState *state);
 int mineCardEffect(int choice1, int choice2, struct gameState *state, int handPos);
+int minionCardEffect(int choice1, int choice2, struct gameState *state, int handPos);
+int tributeCardEffect(struct gameState *state);
 
 
 
