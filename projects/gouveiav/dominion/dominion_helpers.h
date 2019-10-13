5749dad9c14088fbb8124298bd58a272ce632f40
Vanessa Gouveia <gouveiav@oregonstate.edu>
2019-10-12 19:57:35 -0700

    Added separate functions for each card. Added function declarations. Assigned missing variables.

--- projects/gouveiav/dominion/dominion_helpers.h
+++ projects/gouveiav/dominion/dominion_helpers.h
@@ -12,4 +12,9 @@ int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
 
+int baronCardEffect(int choice1, struct gameState *state);
+int minonCardEffect(int choice1, int choice2, struct gameState *state, int handPos);
+int ambassadorCardEffect(int choice1, int choice2, struct gameState *state, int handPos);
+int tributeCardEffect(struct gameState *state);
+int mineCardEffect(int choice1, int choice2, struct gameState *state, int handPos);
 #endif
