dcc9bf7a93d441f0909bd4ab6f7ec32e0dfe72a0
Vanessa Gouveia <gouveiav@oregonstate.edu>
2019-11-25 23:51:54 -0800

    Setting up FinalProject part B for team.

---
+++ projects/FinalProject-Bugs/dominion/unittest1.c
@@ -0,0 +1,23 @@
+#include "rngs.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+int main(int argc, char** argv) {
+
+	printf("Testing Bug1.\n");
+
+
+
+	
+	
+	
+	
+	printf("Test Completed!\n");
+	printf("\n");
+	return 0;
+}
+
