b5a910dfc5fec14703f2873030b47ee3a642fcf3
Vanessa Gouveia <gouveiav@oregonstate.edu>
2019-11-26 23:58:14 -0800

    initial test of bug1

--- projects/FinalProject-Bugs/dominion/unittest1.c
+++ projects/FinalProject-Bugs/dominion/unittest1.c
@@ -1,23 +1,91 @@
 #include "rngs.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
 
 
 
+//Unit test for mine Card
+void testMineCardEffect(int card, int choice1, int choice2, struct gameState *post, int handPos, int currentPlayer) {
+	struct gameState pre;
+	int choice3=0;
+	int i;
+	int *bonus = 0;
+	//make a copy of the gameState to use in comparisons 
+	memcpy(&pre, post, sizeof(struct gameState));
+
+	printf("Testing with choice1: %d  choice2: %d\n", choice1, choice2);
+
+	//calling cardEffect function with 11 = mine card
+	cardEffect(card, choice1, choice2, choice3, post, handPos, bonus);
 
 
-int main(int argc, char** argv) {
+	//if (pre.hand[currentPlayer][choice1] < copper || pre.hand[currentPlayer][choice1] > gold)
+	//{
+	//	printf("First if: return -1\n");
+	//}
+	////card player wants
+	//if (choice2 > treasure_map || choice2 < curse) //bugs here//fixed for coverage
+	//{
+	//	printf("Second if: return -1\n");
+	//}
 
+	if ((getCost(pre.hand[currentPlayer][choice1]) + 3) > getCost(choice2))
+	{
+		printf("Third if: return -1\n"); exit(1);
+	}
+	
+	//printf("Testing handCount: ");
+	//myAssert(pre.handCount[currentPlayer], post->handCount[currentPlayer]);
+	
+	for (i = 0; i < pre.handCount[currentPlayer]; i++)
+	{
+		if (pre.hand[currentPlayer][i] == pre.hand[currentPlayer][choice1])
+		{
+			printf("Card trashed check\n");
+			myAssert(pre.supplyCount[choice1]+1, post->supplyCount[choice1]);
+			break;
+		}
+	}
+	
+
+}
+
+int main() {
+	int choice1, choice2;
+	int treasureCard = 26;
+	int copper = 4;
+	int gold = 6;
+	int handPos = 3;
+	int card = 11; //mine card
+	int k[10] = { adventurer, council_room, feast, gardens, mine,
+			 remodel, smithy, village, baron, great_hall
+	};
+
+	//making a game to test
+	struct gameState G;
 	printf("Testing Bug1.\n");
+	printf("Testing mine Case in cardEffect Function\n");
+	//trash a treasure and gain a T up to 3 more
+	initializeGame(3, k, 22, &G);
 
+	int currentPlayer = whoseTurn(&G);
 
+	//testing mine case w/choice1 between 4 and 6 
+	//testing mine case w/choice2 between 0 and 26
+	choice1 = 3;
+	choice2 = gold;
+	testMineCardEffect(card, choice1, choice2, &G, handPos, currentPlayer);
 
 	
 	
-	
-	
-	printf("Test Completed!\n");
+
+	printf("Test completed!\n");
 	printf("\n");
+
+
 	return 0;
 }
 
