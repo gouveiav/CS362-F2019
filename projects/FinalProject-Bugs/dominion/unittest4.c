5d10e11db80f04bc3b93541a96bfadeea9150e90
Vanessa Gouveia <gouveiav@oregonstate.edu>
2019-11-30 22:03:04 -0800

    uploaded from Lew

--- projects/FinalProject-Bugs/dominion/unittest4.c
+++ projects/FinalProject-Bugs/dominion/unittest4.c
@@ -1,17 +1,39 @@
 #include "rngs.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+
 
 int main(int argc, char** argv) {
  
 
-	printf("Testing Bug4.\n");
+	printf("Testing Bug4 isGameOver().\n");
 
+	struct gameState G;
+	int k[10] = { adventurer, council_room, feast, gardens, mine,
+		 remodel, smithy, salvager, sea_hag, treasure_map
+	};
+	initializeGame(3, k, 48, &G);
+	G.supplyCount[salvager] = 0;
+	G.supplyCount[sea_hag] = 0;
+	G.supplyCount[treasure_map] = 0;
 
+	printf("Last 3 supply piles set to 0, testing isGameOver()\n");
 
+	int test = isGameOver(&G);
+	printf("isGameOver expected outcome: 1 || Actual outcome: %d\n", test);
 
+	printf("2/3 of the Last 3 supply piles set to 0,testing isGameOver()\n");
+	G.supplyCount[adventurer] = 0;
+	G.supplyCount[salvager] = 25;
+	printf("isGameOver expected outcome: 1 || Actual outcome: %d\n", test);
 
-
+	printf("1/3 of the Last 3 supply piles set to 0,testing isGameOver()\n");
+	test = isGameOver(&G);
+	G.supplyCount[council_room] = 0;
+	G.supplyCount[sea_hag] = 25;
+	printf("isGameOver expected outcome: 1 || Actual outcome: %d\n", test);
 
 	printf("Test Completed!\n");
 	printf("\n");
